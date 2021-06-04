7e0afe90620f48edfc3904433e7ee000ccdb04d9
Adriana Scrobote <adriana_100000@hotmail.com>
2021-06-03 21:51:28 -0300

    Iniciando tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -4,13 +4,11 @@ using tabuleiro;
 namespace xadrez_console {
     class Program {
         static void Main(string[] args) {
-            Posicao p;
-
-            p = new Posicao(3, 4);
-
-            Console.WriteLine("Posição " + p);
+            Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 
             Console.ReadLine();
+
+            
         }
     }
 }
---
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro {
+    class Tabuleiro {
+
+        public int linhas { get; set; }
+        public int colunas { get; set; }
+
+        private Peca[,] pecas;
+        
+        public Tabuleiro(int linhas, int colunas) {
+            this.linhas = linhas;
+            this.colunas = colunas;
+            this.pecas = new Peca[linhas, colunas];
+        }
+    }
+}
