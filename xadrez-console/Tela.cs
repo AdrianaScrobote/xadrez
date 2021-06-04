3d7553c720ffc902a64a01b2dbb2611a0adb5136
Adriana Scrobote <adriana_100000@hotmail.com>
2021-06-03 22:08:30 -0300

    Ajuste ao imprimir tabuleiro

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -6,7 +6,7 @@ namespace xadrez_console {
 
         public static void imprimirTela(Tabuleiro tab) {
             for (int i = 0; i < tab.linhas; i++) {
-                for (int j = 0; j < tab.linhas; j++) {
+                for (int j = 0; j < tab.colunas; j++) {
                     if (tab.peca(i, j) == null) {
                         Console.Write("- ");
                     } else {
