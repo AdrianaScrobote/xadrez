6b779b9348fa4b508d81740fce68a67baee69695
Adriana Scrobote <adriana_100000@hotmail.com>
2021-06-05 17:54:51 -0300

    Comecando a colocar pecas no tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using tabuleiro;
+using xadrez;
 
 namespace xadrez_console {
     class Program {
         static void Main(string[] args) {
             Tabuleiro tab = new Tabuleiro(8, 8);
+
+            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 3));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 7));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
 
             Tela.imprimirTela(tab);
 
--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -19,5 +19,10 @@ namespace tabuleiro {
         public Peca peca(int linha, int coluna) {
             return pecas[linha, coluna];
         }
+
+        public void colocarPeca(Peca p, Posicao pos) {
+            pecas[pos.linha, pos.coluna] = p;
+            p.posicao = pos;
+        }
     }
 }
